3e4a274c3e37ff177fb18f1877f664cc64a17ecc
RaulSMuniz <raulanthonymuniz@gmail.com>
2024-09-14 23:23:46 -0400

    NoImplicitAny adicionado juntamente às Tipagens Estáticas

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,10 @@
-import { Negociacao } from './models/negociacao.js';
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 // Nota: o compilador apenas funciona se o código do arquivo TS não tiver erros;
 // Para rodar o compilador: npm run start 
 // (esse comando roda concorrentemente 'npm run server' e 'npm run watch);
-
-const negociacao = new Negociacao(new Date(), 20, 100);
-console.log(negociacao.volume);
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+})
--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,24 +1,24 @@
 export class Negociacao{
-    private _data; // Métodos mais modernos de se declarar uma variável privada em JS;
-    private _quantidade;
-    private _valor;
+    private _data: Date; // Métodos mais modernos de se declarar uma variável privada em JS;
+    private _quantidade: number;
+    private _valor: number;
 
-    constructor(data, quantidade, valor){ // Construtor da classe negociação;
+    constructor(data: Date, quantidade: number, valor: number){ // Construtor da classe negociação;
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
 
-    get data(){ // Retorna a data da negociação;
+    get data(): Date{ // Retorna a data da negociação;
         return this._data;
     }
-    get quantidade(){ // Retorna a quantidade da negociação;
+    get quantidade(): number{ // Retorna a quantidade da negociação;
         return this._quantidade;
     }
-    get valor(){ // Retorna o valor da negociação;
+    get valor(): number{ // Retorna o valor da negociação;
         return this._valor;
     }
-    get volume(){ // Regra de negócio: volume = quantidade * valor da negociação;
+    get volume(): number{ // Regra de negócio: volume = quantidade * valor da negociação;
         return this._quantidade * this._valor;
     }
 }
